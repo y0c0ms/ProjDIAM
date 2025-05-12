bf85b961bc0f92fcba5f38b0a47ba0cbc13151f2
Seu Nome <seu-email@exemplo.com>
2025-05-13 00:07:28 +0100

    Add build.sh to root directory for Render deployment

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Se executando na raiz, vá para o diretório de backend
+cd PriceMapProject/backend
+
+# Instalar dependências
+pip install -r requirements.txt
+
+# Coletar arquivos estáticos
+python manage.py collectstatic --no-input
+
+# Aplicar migrações
+python manage.py migrate 
